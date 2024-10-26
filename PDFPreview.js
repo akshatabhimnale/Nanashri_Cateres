5ed680c9c46f6c27f106934e04f8c88c49a7c913
akshu12345-crypto <akshata.bhimnale@gmail.com>
2024-10-26 17:47:38 +0530

    Rate column is done

--- PDFPreview.js
+++ PDFPreview.js
@@ -1,39 +1,155 @@
 import React, { useRef, useState } from "react";
-import { View, Text, Button, Image, StyleSheet, Alert } from "react-native";
+import { StyleSheet, View, Button, Image } from "react-native";
+import { WebView } from "react-native-webview";
 import { captureRef } from "react-native-view-shot";
 
-const HtmlToJpg = () => {
-  const viewRef = useRef(null);
+const HtmlContentScreen = ({ route }) => {
+  const webViewRef = useRef(null);
   const [imageUri, setImageUri] = useState(null);
 
-  const convertHtmlToJpg = async () => {
+  // Your HTML content as a string
+  const htmlContent = `
+    <html>
+        <head>
+          <style>
+            body { font-family: Arial, sans-serif; padding: 20px }
+            h1 { text-align: center; color: #ED070A; width:100%;}
+            h2 { text-align: center; color: #333; justify-content: center; margin-bottom: 0% }
+            p { margin: 5px 0; }
+            table { width: 40%; border-collapse: collapse; margin-top: 20px; }
+            table, th, td { border: 1px solid black; padding: 10px; text-align: left; }
+            th { background-color: #ffffff; }
+            .footer { position: absolute; bottom: 20px; right: 20px; }
+          </style>
+        </head>
+        <body>
+            <h1 style="justify-content:center; color: black; font-weight:400;font-size:20px"> ${pdfTitle}</h1>
+            <table style="width: 100%; border-collapse: collapse; border-color:black; border: 1px solid">
+            <tr style="display:flex; flex-direction:row; width:100%; border:none;">
+              <th style="border-collapse: collapse; border-right:none;"><img src="data:image/png;base64,${logoBase64}" style="height: 100px; width:100px; "/> </th>
+              <th style="border-collapse: collapse; border-left:none; align-items:center;margin-left:0px"><h1 style="width:620px">Nanashree Caterers</h1></th>
+            </tr>
+            <tr>
+              <td colspan="2"><h2 style="font-weight:200; font-size: 18px; border-collapse: collapse"><strong>Office:</strong> Shubharambh Colony, Near Jijamata Chowk, Walhekarwadi, Chinchwad, Pune - 411033 <br/><strong> Mobile No:</strong>8668976936</h2></td>
+            </tr>
+            <tr style="display:flex; flex-direction:row;">
+              <td colspan="1" style="width:350px; font-size: 18px;"><p><strong>Name:</strong> ${
+                form.name
+              }</p>
+                <p style="font-size: 18px;"><strong>Address:</strong> ${
+                  form.address
+                }</p>
+                <p style="font-size: 18px;"><strong>Mobile No:</strong> ${
+                  form.mobileNumber
+                }</p></td>
+              <td colspan="1" style="width:368px;">
+                <p style="font-size: 18px;"><strong>Date:</strong> ${form.date.toLocaleDateString()}</p>
+                <p style="font-size: 18px;"><strong>Time:</strong> ${form.time.toLocaleTimeString(
+                  [],
+                  {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  }
+                )}</p>
+              </td>
+            </tr>
+</table>
+          <div style="display:flex;">
+          <table style="height:100%; width:746px; margin-top:-1px;margin-right:-7px">
+            <thead>
+              <tr>
+                <th style="text-align: center; width:20%%; font-size: 15px ">Sr No.</th>
+                <th style="text-align: center; width:80%; font-size: 18px; ">Item Name</th>
+              </tr>
+            </thead>
+            <tbody style="justify-content: center;">
+              ${form.menuItems
+                .map(
+                  (item, index) => `
+                <tr style="width:100%">
+                  <td style="border-bottom: none; border-top: none; line-height:3px; text-align: center; font-size: 18px;width:20%">${
+                    index + 1
+                  }</td>
+                  <td style="border: none;line-height:2px; text-align: center; font-size: 18px; width:80%">${
+                    item.itemName
+                  }</td>    
+                </tr>`
+                )
+                .join("")}
+            </tbody>
+          </table>
+          <table style="width:100%; margin-top:-1px; margin-left:0px; border-left:none">
+                <thead>
+                    <tr>
+                        <th style="text-align: center; font-size: 18px">Quantity</th>
+                        <th style="text-align: center; font-size: 18px">Total Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style="text-align: center; font-size: 18px; border-left:none;margin-left:10px;">${
+                          form.quantityOfPeople
+                        }</td>
+                        <td style="text-align: center; font-size: 18px; border-left:none;margin-left:0px;">${
+                          form.totalAmount
+                        } Rs Only</td>
+                    </tr>
+                </tbody>
+          </table>
+          </div>
+          <table style="width:100%; margin-top:-1px">
+                <thead>
+                    <tr>
+                        <th style="text-align: center; font-size: 18px">Other Items</th>
+                        <th style="text-align: center; font-size: 18px">Grand Total Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style="text-align: left; width:49%">
+                        <h3 style="font-size: 18px; margin-top: 4px">Tables : ${
+                          form.quantityOfTable
+                        }</h3>
+                        <h3 style="font-size: 18px">Chairs : ${
+                          form.quantityOfChair
+                        }</h3></td>
+                        <td style="text-align: center; width:50%; font-size: 18px">${
+                          form.totalAmount
+                        } Rs Only</td>
+                    </tr>
+                </tbody>
+          </table>
+       
+        </body>
+      </html>
+  `;
+
+  const convertHtmlToImage = async () => {
     try {
-      const uri = await captureRef(viewRef, {
+      const uri = await captureRef(webViewRef, {
         format: "jpg",
         quality: 1,
       });
       setImageUri(uri);
     } catch (error) {
-      console.error("Error generating image:", error);
-      Alert.alert("Error", "An error occurred while generating the image.");
+      console.error("Error capturing image:", error);
     }
-    };
-    
-    
+  };
 
   return (
     <View style={styles.container}>
-      {/* View that contains the HTML content */}
-      <View ref={viewRef} style={styles.htmlContent}>
-        <View style={styles.content}>
-          <Text style={styles.title}>Sample HTML Content</Text>
-          <Text>This content will be converted to a JPG image.</Text>
-        </View>
+      {/* WebView to render HTML content */}
+      <View style={styles.webViewContainer} ref={webViewRef}>
+        <WebView
+          originWhitelist={["*"]}
+          source={{ html: htmlContent }}
+          style={styles.webView}
+        />
       </View>
 
-      <Button title="Convert to JPG" onPress={convertHtmlToJpg} />
+      <Button title="Convert to Image" onPress={convertHtmlToImage} />
 
-      {/* Display the generated JPG image */}
+      {/* Display the generated image */}
       {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
     </View>
   );
@@ -46,20 +162,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 20,
   },
-  htmlContent: {
+  webViewContainer: {
     width: "100%",
-    padding: 20,
-    backgroundColor: "#fff",
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 10,
+    height: "70%", // Adjust height as needed
   },
-  content: {
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
+  webView: {
+    width: "100%",
+    height: "100%",
   },
   image: {
     width: 300,
@@ -68,4 +177,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default HtmlToJpg;
+export default HtmlContentScreen;
--- Quotation.js
+++ Quotation.js
@@ -1,4 +1,4 @@
-import React, { useRef,useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -10,8 +10,11 @@ import {
   Image,
   StyleSheet,
 } from "react-native";
+
+import { captureRef } from "react-native-view-shot";
 import { useNavigation } from "@react-navigation/native";
 import * as Print from "expo-print";
+import { WebView } from "react-native-webview";
 import * as FileSystem from "expo-file-system";
 import * as Sharing from "expo-sharing";
 import { Asset } from "expo-asset";
@@ -19,6 +22,8 @@ import DateTimePicker from "@react-native-community/datetimepicker";
 
 const NanashriCaterersForm = ({ route }) => {
   const { formTitle, pdfTitle } = route.params;
+  const webViewRef = useRef(null);
+  const [imageUri, setImageUri] = useState(null);
   const [form, setForm] = useState({
     name: "",
     address: "",
@@ -84,8 +89,6 @@ const NanashriCaterersForm = ({ route }) => {
       return;
     }
 
-    
-
     const htmlContent = `
       <html>
         <head>
@@ -104,14 +107,14 @@ const NanashriCaterersForm = ({ route }) => {
             <h1 style="justify-content:center; color: black; font-weight:400;font-size:20px"> ${pdfTitle}</h1>
             <table style="width: 100%; border-collapse: collapse; border-color:black; border: 1px solid">
             <tr style="display:flex; flex-direction:row; width:100%; border:none;">
-              <th style="border-collapse: collapse; border-right:none;"><img src="data:image/png;base64,${logoBase64}" style="height: 100px; width:100px; "/> </th>
-              <th style="border-collapse: collapse; border-left:none; align-items:center;margin-left:0px"><h1 style="width:620px">Nanashree Caterers</h1></th>
+              <th style="border-collapse: collapse; border:none;"><img src="data:image/png;base64,${logoBase64}" style="height: 100px; width:100px; "/> </th>
+              <th style="border-collapse: collapse; border:none; align-items:center;margin-left:0px"><h1 style="width:600px">Nanashree Caterers</h1></th>
             </tr>
-            <tr>
+            <tr style="width:600px">
               <td colspan="2"><h2 style="font-weight:200; font-size: 18px; border-collapse: collapse"><strong>Office:</strong> Shubharambh Colony, Near Jijamata Chowk, Walhekarwadi, Chinchwad, Pune - 411033 <br/><strong> Mobile No:</strong>8668976936</h2></td>
             </tr>
-            <tr style="display:flex; flex-direction:row;">
-              <td colspan="1" style="width:350px; font-size: 18px;"><p><strong>Name:</strong> ${
+            <tr style="display:flex; flex-direction:row; ">
+              <td colspan="1" style="width:434px; font-size: 18px; border:none"><p><strong>Name:</strong> ${
                 form.name
               }</p>
                 <p style="font-size: 18px;"><strong>Address:</strong> ${
@@ -120,24 +123,18 @@ const NanashriCaterersForm = ({ route }) => {
                 <p style="font-size: 18px;"><strong>Mobile No:</strong> ${
                   form.mobileNumber
                 }</p></td>
-              <td colspan="1" style="width:368px;">
-                <p style="font-size: 18px;"><strong>Date:</strong> ${form.date.toLocaleDateString()}</p>
-                <p style="font-size: 18px;"><strong>Time:</strong> ${form.time.toLocaleTimeString(
-                  [],
-                  {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  }
-                )}</p>
+              <td colspan="1" style="width:278px; border-right:none; border-top:none; border-bottom:none">
+                <p style="font-size: 18px;"><strong>Date:</strong> ${formattedDate}</p>
+                <p style="font-size: 18px;"><strong>Time:</strong> ${formattedTime}</p>
               </td>
             </tr>
-</table>
+        </table>
           <div style="display:flex;">
-          <table style="height:100%; width:746px; margin-top:-1px;margin-right:-7px">
+          <table style="height:100%; width:758px; margin-top:-1px;margin-right:-7px;border-right:none">
             <thead>
               <tr>
-                <th style="text-align: center; width:20%%; font-size: 15px ">Sr No.</th>
-                <th style="text-align: center; width:80%; font-size: 18px; ">Item Name</th>
+                <th style="text-align: center;  font-size: 15px ">Sr No.</th>
+                <th style="text-align: center;  font-size: 18px; ">Item Name</th>
               </tr>
             </thead>
             <tbody style="justify-content: center;">
@@ -145,10 +142,10 @@ const NanashriCaterersForm = ({ route }) => {
                 .map(
                   (item, index) => `
                 <tr style="width:100%">
-                  <td style="border-bottom: none; border-top: none; line-height:3px; text-align: center; font-size: 18px;width:20%">${
+                  <td style="border-bottom: none; border-top: none; line-height:3px; text-align: center; font-size: 18px;width:20%;">${
                     index + 1
                   }</td>
-                  <td style="border: none;line-height:2px; text-align: center; font-size: 18px; width:80%">${
+                  <td style="border: none;line-height:2px; text-align: center; font-size: 18px;">${
                     item.itemName
                   }</td>    
                 </tr>`
@@ -156,15 +153,19 @@ const NanashriCaterersForm = ({ route }) => {
                 .join("")}
             </tbody>
           </table>
-          <table style="width:100%; margin-top:-1px; margin-left:0px; border-left:none">
+          <table style="width:100%; margin-top:-1px; margin-left:0px;">
                 <thead>
                     <tr>
-                        <th style="text-align: center; font-size: 18px">Quantity</th>
+                        <th style="text-align: center; font-size: 18px">Rate</th>
+                        <th style="text-align: center; font-size: 18px; width:30%;margin-left:-20px">Quantity</th>
                         <th style="text-align: center; font-size: 18px">Total Amount</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
+                    <td style="text-align: center; font-size: 18px; border-left:none;margin-left:10px;">${
+                      form.totalAmount / form.quantityOfPeople
+                    }</td>
                         <td style="text-align: center; font-size: 18px; border-left:none;margin-left:10px;">${
                           form.quantityOfPeople
                         }</td>
@@ -197,12 +198,16 @@ const NanashriCaterersForm = ({ route }) => {
                     </tr>
                 </tbody>
           </table>
-       
         </body>
       </html>
     `;
 
     try {
+      // const {uriImg} = await captureRef(webViewRef, {
+      //   format: "jpg",
+      //   quality: 1,
+      // });
+      // setImageUri(uriImg);
 
       const { uri } = await Print.printToFileAsync({ html: htmlContent });
       const pdfFileUri = `${FileSystem.documentDirectory}NanashriCaterersReceipt.pdf`;
@@ -250,6 +255,22 @@ const NanashriCaterersForm = ({ route }) => {
       Alert.alert("Limit Reached", "You can only add up to 50 menu items.");
     }
   };
+  const convertHtmlToImage = async () => {
+    try {
+      const uri = await captureRef(webViewRef, {
+        format: "jpg",
+        quality: 1,
+      });
+      setImageUri(uri);
+    } catch (error) {
+      console.error("Error capturing image:", error);
+    }
+  };
+  const formattedDate = form.date.toLocaleDateString();
+  const formattedTime = form.time.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 
   return (
     <ScrollView style={styles.container}>
@@ -399,6 +420,126 @@ const NanashriCaterersForm = ({ route }) => {
         value={form.totalAmount}
         onChangeText={(text) => handleInputChange("totalAmount", text)}
       />
+
+      <View style={styles.webViewContainer} ref={webViewRef}>
+        <WebView
+          style={styles.webView}
+          source={{
+            html: `
+        <html>
+        <head>
+          <style>
+            body { font-family: Arial, sans-serif; padding: 20px }
+            h1 { text-align: center; color: #ED070A; width:100%;}
+            h2 { text-align: center; color: #333; justify-content: center; margin-bottom: 0% }
+            p { margin: 5px 0; }
+            table { width: 40%; border-collapse: collapse; margin-top: 20px; }
+            table, th, td { border: 1px solid black; padding: 10px; text-align: left; }
+            th { background-color: #ffffff; }
+            .footer { position: absolute; bottom: 20px; right: 20px; }
+          </style>
+        </head>
+        <body>
+            <h1 style="justify-content:center; color: black; font-weight:400;font-size:20px"> ${pdfTitle}</h1>
+            <table style="width: 100%; border-collapse: collapse; border-color:black; border: 1px solid">
+            <tr style="display:flex; flex-direction:row; width:100%; border:none;">
+              <th style="border-collapse: collapse; border-right:none;"><img src="data:image/png;base64,${logoBase64}" style="height: 100px; width:100px; "/> </th>
+              <th style="border-collapse: collapse; border-left:none; align-items:center;margin-left:0px"><h1 style="width:782px">Nanashree Caterers</h1></th>
+            </tr>
+            <tr>
+              <td colspan="2"><h2 style="font-weight:200; font-size: 18px; border-collapse: collapse"><strong>Office:</strong> Shubharambh Colony, Near Jijamata Chowk, Walhekarwadi, Chinchwad, Pune - 411033 <br/><strong> Mobile No:</strong>8668976936</h2></td>
+            </tr>
+            <tr style="display:flex; flex-direction:row;">
+              <td colspan="1" style="width:436px; font-size: 18px;"><p><strong>Name:</strong> ${
+                form.name
+              }</p>
+                <p style="font-size: 18px;"><strong>Address:</strong> ${
+                  form.address
+                }</p>
+                <p style="font-size: 18px;"><strong>Mobile No:</strong> ${
+                  form.mobileNumber
+                }</p></td>
+              <td colspan="1" style="width:445px;">
+                <p style="font-size: 18px;"><strong>Date:</strong> ${formattedDate}</p>
+                <p style="font-size: 18px;"><strong>Time:</strong> ${formattedTime}</p>
+              </td>
+            </tr>
+        </table>
+          <div style="display:flex;">
+          <table style="height:100%; width:920px; margin-top:-1px;margin-right:-7px;border-right:none">
+            <thead>
+              <tr>
+                <th style="text-align: center;  font-size: 15px ">Sr No.</th>
+                <th style="text-align: center;  font-size: 18px; ">Item Name</th>
+              </tr>
+            </thead>
+            <tbody style="justify-content: center;">
+              ${form.menuItems
+                .map(
+                  (item, index) => `
+                <tr style="width:100%">
+                  <td style="border-bottom: none; border-top: none; line-height:3px; text-align: center; font-size: 18px;width:20%;">${
+                    index + 1
+                  }</td>
+                  <td style="border: none;line-height:2px; text-align: center; font-size: 18px;">${
+                    item.itemName
+                  }</td>    
+                </tr>`
+                )
+                .join("")}
+            </tbody>
+          </table>
+          <table style="width:100%; margin-top:-1px; margin-left:0px;">
+                <thead>
+                    <tr>
+                        <th style="text-align: center; font-size: 18px">Rate</th>
+                        <th style="text-align: center; font-size: 18px; width:30%;margin-left:-20px">Quantity</th>
+                        <th style="text-align: center; font-size: 18px">Total Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td style="text-align: center; font-size: 18px; border-left:none;margin-left:10px;">${
+                      form.totalAmount / form.quantityOfPeople
+                    }</td>
+                        <td style="text-align: center; font-size: 18px; border-left:none;margin-left:10px;">${
+                          form.quantityOfPeople
+                        }</td>
+                        <td style="text-align: center; font-size: 18px; border-left:none;margin-left:0px;">${
+                          form.totalAmount
+                        } Rs Only</td>
+                    </tr>
+                </tbody>
+          </table>
+          </div>
+          <table style="width:100%; margin-top:-1px">
+                <thead>
+                    <tr>
+                        <th style="text-align: center; font-size: 18px">Other Items</th>
+                        <th style="text-align: center; font-size: 18px">Grand Total Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style="text-align: left; width:49%">
+                        <h3 style="font-size: 18px; margin-top: 4px">Tables : ${
+                          form.quantityOfTable
+                        }</h3>
+                        <h3 style="font-size: 18px">Chairs : ${
+                          form.quantityOfChair
+                        }</h3></td>
+                        <td style="text-align: center; width:50%; font-size: 18px">${
+                          form.totalAmount
+                        } Rs Only</td>
+                    </tr>
+                </tbody>
+          </table>
+        </body>
+      </html>`,
+          }}
+        />
+      </View>
+      {imageUri && <Image source={{ uriImg: imageUri }} style={styles.image} />}
       <TouchableOpacity style={styles.button} onPress={generatePDF}>
         <Text style={styles.buttonText}>Generate PDF</Text>
       </TouchableOpacity>
@@ -477,6 +618,19 @@ const styles = StyleSheet.create({
   backButtonText: {
     color: "#fff",
     fontSize: 16,
+  },
+  webViewContainer: {
+    marginTop:10,
+    width: 350,
+    height: 400, // Adjust height as needed
+  },
+  webView: {
+    width: 350,
+    height: 400,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 
